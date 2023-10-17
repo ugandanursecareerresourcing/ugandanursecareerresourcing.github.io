7821d52b94f1d08821902a5aafca4c9178d4608d
rogers <variablecode404@gmail.com>
2023-10-17 23:39:32 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,343 +1,88 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+$(document).ready(()=>{
+	let fake = $('#fake').text();
 
-(function($) {
+	$('#nav').html(`
+		<nav class="p-0 flex sticky-top bg-white justify-center align-items-center">
+			<div class="flex flex-wrap-sm justify-between align-items-center col-8-xxl px-15-xxl px-4-lg col-10-md px-2-md">
+				<div class="flex justify-between col-10-sm  align-items-center">
+					<a href="${fake}index.html"><div class="flex align-items-center">
+						<p class="m-0">
+							<img src="${fake}images/logo.svg" class="img pr-2" style="height: 37px;">
+						</p>
+						<p class="logo m-0 py-2">Frankie Res</p>
+					</div></a>
+					<p id="togglemore" class="icon-menu text-xl-7 px-2 py-2-xxl m-0 d-none-xxl d-block-sm"></p>
+				</div>
+				<div id="more" class="flex d-none-sm col-10-sm flex-wrap-sm align-items-center justify-end">
+					<p class="m-0 col-10-sm py-3 px-2"><a href="${fake}index.html">Home</a></p>
+					<p class="m-0 col-10-sm py-3 px-2"><a href="${fake}index.html#blogs">Blog Pages</a></p>
+					<p class="m-0 col-10-sm py-3 px-2"><a href="${fake}index.html#services">Find Service</a></p>
+					<p class="m-0 col-10-sm py-3 px-2"><a href="#ex1" rel="modal:open">Contact Us</a></p>
+					<p class="m-0 col-10-sm py-3 px-2"><a href="#ex1" rel="modal:open">Check Our Reseach</a></p>
+					<p class="m-0 col-10-sm py-2 px-2"><a href="#ex1" rel="modal:open">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="30px" width="30px">
+						  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+						</svg>
+					</a></p>
+				</div>
+			</div>
+		</nav>
 
-	"use strict";
-
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
-
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
-
-	// Scrollax
-   $.Scrollax();
+	`);
 
 
-
-   // Burger Menu
-	var burgerMenu = function() {
-
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
-
-			event.preventDefault();
-
-			if ( $('#ftco-nav').is(':visible') ) {
-				$(this).removeClass('active');
-			} else {
-				$(this).addClass('active');	
-			}
-
-			
-			
-		});
-
-	};
-	burgerMenu();
+	$('#togglemore').click(()=>{
+		$('#more').fadeToggle();
+	})
 
 
-	var onePageClick = function() {
+	$('#footer').html(`
+		<footer class="flex flex-wrap justify-center py-10-xxl py-5-sm">
+			<div class="col-8-xxl col-9-lg col-10-md px-2-md px-3-sm flex flex-wrap-md ">
+				<div class="col-3-xxl col-10-sm">
+					<div class="flex align-items-center">
+						<p class="m-0">
+							<img src="${fake}images/logo.svg" class="img pr-2" style="height: 37px;">
+						</p>
+						<p class="logo m-0 py-2 text-black b5">Frankie Res</p>
+					</div>
+				</div>
+				<div class="col-3-xxl col-10-sm">
+					<p class="m-0 bold mb-1 pt-4-sm">ON PAGE</p>
+					<a href="${fake}index.html"><p class="m-0 py-1">Home</p></a>
+					<a href="${fake}index.html#blogs"><p class="m-0 py-1">Blog Pages</p></a>
+					<a href="${fake}index.html#services"><p class="m-0 py-1">Find Services</p></a>
+					<a href="#ex1" rel="modal:open"><p class="m-0 py-1">Contact Us</p></a>
+					<a href="${fake}index.html"><p class="m-0 py-1">Check Out Research</p></a>
+				</div>
+				<div class="col-3-xxl col-10-sm">
+					<p class="m-0 bold mb-1 pt-4-sm">USEFUL LINKS</p>
+					<a href=""><p class="m-0 py-1">Nurses</p></a>
+					<a href=""><p class="m-0 py-1">Care Assistants</p></a>
+					<a href="${fake}blogs/ielts.html"><p class="m-0 py-1">IELTS</p></a>
+					<a href="${fake}blogs/oet.html"><p class="m-0 py-1">OET</p></a>
+					<a href="${fake}blogs/cbt.html"><p class="m-0 py-1">CBT</p></a>
+				</div>
+				<div class="col-3-xxl col-10-sm">
+					<p class="m-0 bold mb-1 pt-4-sm">CONTACT US</p>
+					<p class="m-0 py-1"><i class="icon-whatsapp"></i> &nbsp;+447960886147</p>
+					<p class="m-0 py-1"><i class="icon-email"></i> &nbsp;frankieres@gmail.com</p>
 
+					<p class="m-0 pb-3 pt-4 bold uppercase">Follows Us</p>
+					<div class="flex justify-start flex-wrap">
+						<a href=""><p class="m-0 px-2"><i class="text-xl-3 icon-facebook"></i></p></a>
+						<a href=""><p class="m-0 px-2"><i class="text-xl-3 icon-instagram"></i></p></a>
+						<a href=""><p class="m-0 px-2"><i class="text-xl-3 icon-twitter"></i></p></a>
+						<a href=""><p class="m-0 px-2"><i class="text-xl-3 icon-youtube"></i></p></a>
+					</div>
+				</div>
+			</div>
 
-		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
-	    event.preventDefault();
+			<div class="col-8-xxl col-10-md mt-3 up px-3 flex justify-between">
+				<p class="m-0 py-3 px-2-xxl px-1-sm">latest at Oct 2023</p>
+				<p class="m-0 py-3 px-2-xxl px-1-sm">Built by <a href="">Variable Code</a></p>
+			</div>
+		</footer>
+	`)
 
-	    var href = $.attr(this, 'href');
-
-	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
-	    }, 500, function() {
-	    	// window.location.hash = href;
-	    });
-		});
-
-	};
-
-	onePageClick();
-	
-
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-		$('.carousel-properties').owlCarousel({
-			autoplay: true,
-			center: false,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-		$('.carousel-agent').owlCarousel({
-			autoplay: true,
-			center: false,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-		$('.carousel-testimony').owlCarousel({
-			autoplay: true,
-			autoHeight: true,
-			center: true,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			dots: true,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-
-	};
-	carousel();
-
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
-
-
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
-
-	// scroll
-	var scrollWindow = function() {
-		$(window).scroll(function(){
-			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
-
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
-				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
-	};
-	scrollWindow();
-
-	
-
-	var counter = function() {
-		
-		$('#section-counter, .hero-wrap, .ftco-counter').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
-	}
-	counter();
-
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .ftco-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
-
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
-
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-
-
-
-
-
-})(jQuery);
-
+})
